7da64c2a8bb6581103c0906c71ecb7e61da7ba41
María Viriginia castillo <127612492+vircastillo75@users.noreply.github.com>
2024-06-14 18:30:43 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {Header} from "./components/Header"
+
+
+
+function App() {
+
+
+  return (
+    <>
+     <Header />
+   
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react"
+
+export const Header = ()=>{
+
+    const[darkMode, setDarkMode] = useState(false)
+
+    const handleClick = () =>{
+        setDarkMode(!darkMode)
+    }
+
+    useEffect(()=>{
+        if(darkMode){
+            document.documentElement.classList.add("dark")
+        }else{
+            document.documentElement.classList.remove("dark")
+        }
+    },[darkMode])
+
+    return(
+        <header className="bg-Very-Pale-Blue dark:bg-Very-Dark-Blue h-[235px] rounded-b-[20px] pt-8 px-6 mb-[1000px] md:mb-[480px] xl:mb-[200px]">
+        <h1 className="text-Very-Dark-Blue dark:text-Very-Pale-Blue text-2xl font-bold mb-1">Social Media Dashboard</h1>
+        <p className="text-Dark-Grayish-Blue dark:text-Desaturated-Blue font-bold mb-6">Total Followers: 23,004</p>
+        <hr className="bg-black mb-[19px]"/>
+        <div className="flex justify-between">
+            <p className="text-Dark-Grayish-Blue  dark:text-Desaturated-Blue font-bold"> Dark Mode</p>
+            <label htmlFor="darkMode" className="border relative bg-Toggle w-12 h-6 rounded-full overflow-hidden cursor-pointer p-[3px]">
+                <input onClick={handleClick} id="darkMode" type="checkbox" className="peer sr-only"/>
+                <div className="w-full h-full peer-checked:bg-Toggle-Gradient absolute top-0 left-0"></div>
+                <div className="w-[18px] h-[18px] bg-Light-Grayish-Blue rounded-full peer-checked:translate-x-[24px] transition-all"></div>
+            </label>
+        </div>
+    </header>
+    )
+}
